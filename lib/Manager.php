0b1b7a2b559ce479ace43dca909679798cdad1cb
Joas Schilling <coding@schilljs.com>
2019-06-27 20:18:57 +0200

    Read and allow to set the lobby state
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/Manager.php
+++ lib/Manager.php
@@ -126,7 +126,7 @@ class Manager {
 			]));
 		}
 
-		return new Room($this, $this->db, $this->secureRandom, $this->dispatcher, $this->timeFactory, $this->hasher, (int) $row['id'], (int) $row['type'], (int) $row['read_only'], $row['token'], $row['name'], $row['password'], (int) $row['active_guests'], $activeSince, $lastActivity, $lastMessage, (string) $row['object_type'], (string) $row['object_id']);
+		return new Room($this, $this->db, $this->secureRandom, $this->dispatcher, $this->timeFactory, $this->hasher, (int) $row['id'], (int) $row['type'], (int) $row['read_only'], (int) $row['lobby_state'], $row['token'], $row['name'], $row['password'], (int) $row['active_guests'], $activeSince, $lastActivity, $lastMessage, (string) $row['object_type'], (string) $row['object_id']);
 	}
 
 	/**
